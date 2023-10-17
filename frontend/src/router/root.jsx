4db3505aabb3d4d66717264d3c00fccb3cf554fe
Henry Milac <henrymilacprog@gmail.com>
2023-10-17 09:45:11 -0500

    DashboardPage added

--- frontend/src/router/root.jsx
+++ frontend/src/router/root.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../features/Home/pages";
+import DashboardPage from "../features/Dashboard/pages";
 
 export const router = createBrowserRouter([
     {
         path:"/",
         element: <HomePage />,
+    },
+    {
+        path:"/dashboard",
+        element: <DashboardPage/>
     },
     {
         path:"/login",
