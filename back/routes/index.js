3caf7af7736fccce6891e02e1f64435a785c1595
Gustavo Torres <95319442+betogus@users.noreply.github.com>
2023-10-17 22:18:07 -0300

    Update index.js

--- back/routes/index.js
+++ back/routes/index.js
@@ -6,7 +6,7 @@ const app = Express();
 const usuarioRoutes = require('./usuario-routes');
 const authRoutes = require('./auth-routes');
 //const localidadRoutes = require('./localidad-routes')
-//const eventoRoutes = require('./evento-routes')
+const eventRoutes = require('./event-routes')
 //const postRoutes = require('./post-routes')
 
 
