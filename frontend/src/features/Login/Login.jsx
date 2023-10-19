59606190fa6799906a13db6594e730f145901a77
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-10-19 16:01:19 -0600

    adding validations

--- frontend/src/features/Login/Login.jsx
+++ frontend/src/features/Login/Login.jsx
@@ -3,36 +3,54 @@ import axios from "axios"
 
 const Login = () =>{
     
-    const {handleSubmit,register} = useForm()
+    const {handleSubmit,register,formState: {errors}} = useForm()
+
+    console.log("error",errors)
 
     const onSubmit = (data) =>{
         console.log(data)
-        axios.post("https://s11-06-n-node-react-back.onrender.com/usuarios/login",data).then(resp => {
+        axios.post("https://s11-06-n-node-react-back.onrender.com/users/login",data).then(resp => {
             console.log(resp)
         })
     }
 
     return (
         <>
-            <form action="#" onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label htmlFor="">Nombre: </label> 
-                    <input type="text" {...register("nombre")}/>
-                </div>
-                <div>
-                    <label htmlFor="">Apellido: </label> 
-                    <input type="text" {...register("apellido")}/>
-                </div>
+            <form action="#" onSubmit={handleSubmit(onSubmit)} id='form'>
                 <div>
                     <label htmlFor="">Email: </label> 
-                    <input type="text" {...register("email")}/>
+                    <input type="email" {...register("email", 
+                    {
+                        required:{
+                            value: true,
+                            message:"Correo requerido"
+                        },
+                        pattern:{
+                            value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                            message:"Correo no valido"
+                        }
+                    })}
+                    />
+                    
+                    {errors.email && <span>{errors.email.message}</span>}
                 </div>
+
                 <div>
                     <label htmlFor="">Password: </label> 
-                    <input type="text" {...register("password")}/>
+                    <input type="text" {...register("password",
+                    {
+                        required:{
+                            value: true,
+                            message:"Ingresar contraseña por favor"
+                        }
+                        }
+                        )}/>
+                    {errors.password && <span>{errors.password.message}</span>}
                 </div>
 
                 <button type='submit'>Login</button>
+
+                <p className='warnings' id='warnings'></p>
 
             </form>
         </>
