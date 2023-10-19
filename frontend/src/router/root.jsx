d597023ea06b7fbee2d2509779b4c398e4e690b8
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-10-19 11:25:13 -0600

    .

--- frontend/src/router/root.jsx
+++ frontend/src/router/root.jsx
@@ -1,11 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../features/Home/pages";
 import DashboardPage from "../features/Dashboard/pages";
-<<<<<<< HEAD
 import Login from "../features/Login/Login";
-=======
 import Noticias from "../features/Noticias/pages/Noticias";
->>>>>>> 3e4e800e6932a322fde0e7187aec44103c40b8fb
 
 export const router = createBrowserRouter([
     {
