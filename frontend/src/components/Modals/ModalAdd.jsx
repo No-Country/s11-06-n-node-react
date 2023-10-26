22136bcb703f9ac755c24d7cde2b70891f0e366d
Alfonso Alexander Briones Ventura <alexanderbvart@gmail.com>
2023-10-25 21:40:10 -0500

    feat: modales para crear eventos-noticias-grupos

--- frontend/src/components/Modals/ModalAdd.jsx
+++ frontend/src/components/Modals/ModalAdd.jsx
@@ -9,7 +9,7 @@ import image1 from '../../../public/user1.jpeg'
 import { BsPostcard } from 'react-icons/bs';
 // import ModalAddEvent from '../Modals/ModalAddEvent';
 
-export default function ModalAdd({ children, title }) {
+const ModalAdd = ({ children, title, icon }) => {
 
     const [modal, setModal] = useState(false)
     const openModal = () => {
@@ -22,26 +22,23 @@ export default function ModalAdd({ children, title }) {
     return (
 
         <>
-            <button onClick={openModal} className='w-14 h-14 bg-greenSecundary rounded-full m-1 absolute -top-96  -right-14 text-white flex flex-col justify-center items-center'>
-                <span className='text-xl'><BsPostcard    /></span>
+            <button onClick={openModal} className='w-16 h-16 p-2 bg-greenSecundary rounded-full text-white flex flex-col justify-center items-center border-solid shadow-2xl border-greenPrimary hover:bg-greenPrimary'>
+                <span className='text-xl'> {icon}</span>
                 <p className='text-xs'>{title}</p>
             </button>
+            
             <Modal isOpen={modal} onRequestClose={closeModal} className={'overflow-y-scroll h-full'}>
                 <div className='flex justify-center items-center h-full text-sm text-greenPrimary'>
                     <div className='border border-graySecundary max-w-xs sm:max-w-xl w-full p-5 sm:p-14 shadow-2xl bg-white rounded-lg relative'>
+                        
+                        {/* Header */}
                         <div className='border-b border-greenPrimary flex justify-between'>
                             <p className='text-2xl py-2'>{title}</p>
                             <button onClick={closeModal} className='absolute top-5 right-5 text-2xl text-greenSecundary'><GrClose /></button>
                         </div>
-                        <div className='flex gap-x-3 items-center my-5'>
-                            <div>
-                                <ImageProfileUserSmall imagen={image1} />
-                            </div>
-                            <div>
-                                <p className='font-bold mb-1'>Henry Ramirez</p>
-                                <p className="border border-gray-500 px-2 py-1 rounded-2xl flex items-center w-fit text-xs"><p className='mr-1 text-base -ml-1'><CiLocationOn /></p>Lima</p>
-                            </div>
-                        </div>
+
+                        {/* Body */}
+                        {/*  */}
 
                         {children}
 
@@ -52,3 +49,34 @@ export default function ModalAdd({ children, title }) {
 
     )
 }
+
+
+const Header = ({ className, children }) => {
+    return (
+        <header className={`${className} w-full `}>
+            {children}
+        </header>
+    )
+}
+const Body = ({ className, children }) => {
+    return (
+        <header className={`${className} w-full `}>
+            {children}
+        </header>
+    )
+}
+
+const Footer = ({ className, children }) => {
+    return (
+        <footer className={`${className} w-full align-bottom  mt-auto`}>
+            {children}
+        </footer>
+    )
+}
+
+
+ModalAdd.Header = Header;
+ModalAdd.Body = Body;
+ModalAdd.Footer = Footer;
+
+export default ModalAdd
