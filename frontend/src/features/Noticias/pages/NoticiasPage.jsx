3860d629040fcd5999cc2da433f913b690daebc5
Henry Milac <henrymilacprog@gmail.com>
2023-10-26 13:12:07 -0500

    Preguntas frecuentes done

--- frontend/src/features/Noticias/pages/NoticiasPage.jsx
+++ frontend/src/features/Noticias/pages/NoticiasPage.jsx
@@ -1,4 +1,5 @@
 import CardNoticia from '../../../components/Cards/CardNoticia/CardNoticia'
+import ListCardFirends from '../../../components/ListCardFriends'
 import SearchBar from '../../../components/SearchBar/SearchBar'
 import Sidebar from '../../../components/Sidebar/Sidebar'
 
@@ -8,20 +9,19 @@ export default function NoticiasPage() {
         <div className='flex'>
             <div className='hidden md:block'><Sidebar/></div>
 
-            <div className="mb-20 ml-0 md:ml-64 px-5 md:px-10 mx-auto w-full">
-                <div className=' max-w-7xl mx-auto'>
+
+            <div className="mb-20 ml-0 lg:ml-64 px-5 md:px-10 mx-auto w-full">
+                <div className='max-w-7xl mx-auto'>
                     <SearchBar/>
                     <h1 className='text-2xl font-bold my-10'>Noticias:</h1>
-
-                    <div className='flex flex-col gap-10 md:gap-y-20'>
-                        <CardNoticia/>
-                        <CardNoticia/>
-                        <CardNoticia/>
-                        <CardNoticia/>
-                    </div>
+                    <CardNoticia/>
+                    <CardNoticia/>
+                    <CardNoticia/>
+                    <CardNoticia/>
                 </div>
             </div>
-        </div>
 
+            <div className='hidden lg:block'><ListCardFirends/></div>
+        </div>
     )
 }
