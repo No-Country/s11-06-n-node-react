2af572707262a67b58cc88938f0b8132d6bcc14b
JaviPeder~ <javierpedernera@gmail.com>
2023-10-27 21:13:04 -0300

    endpoints actualizados

--- frontend/src/Redux/Actions/EventGet.js
+++ frontend/src/Redux/Actions/EventGet.js
@@ -8,7 +8,7 @@ import axios from "axios";
 const getAllEvents= () => {
 	return async (dispatch) => {
 		try {
-			const dbData = (await axios.get(`https://s11-06-n-node-react-back.onrender.com/documentation/#/`)).data;			
+			const dbData = (await axios.get(`https://s11-06-n-node-react-back.onrender.com/events`)).data;			
 			return dispatch(getEvents(dbData));
 		} catch (error) {
 			alert({error: error.message});
@@ -22,7 +22,7 @@ const getAllEvents= () => {
 const getEventDetail = (id) => {
 	return async (dispatch) => {
 		try {
-			const dbData = (await axios.get(`https://s11-06-n-node-react-back.onrender.com/documentation/#/${id}`)).data;
+			const dbData = (await axios.get(`https://s11-06-n-node-react-back.onrender.com/events/${id}`)).data;
 			dispatch(getDetailEvent(dbData));
 		} catch (error) {
 			alert({error: error.message});
@@ -34,7 +34,7 @@ const getEventDetail = (id) => {
 const eventPost = (eventData) => {
   return async (dispatch) => {
     try {
-       const response = await axios.post('https://s11-06-n-node-react-back.onrender.com/documentation/#/', eventData);
+       const response = await axios.post('https://s11-06-n-node-react-back.onrender.com/events', eventData);
        dispatch(postEvent(response.data));
     } catch (error) {
       console.error(error);
@@ -51,7 +51,7 @@ const modifyTheEvent = (id, event) => {
             if (!event) {
                 throw new Error("Event is undefined.");
               }
-            const dbData = await axios.put(`https://s11-06-n-node-react-back.onrender.com/documentation/#/${id}`, news);
+            const dbData = await axios.put(`https://s11-06-n-node-react-back.onrender.com/events/${id}`, event);
             return dispatch(modifyEvent(dbData));
         } catch (error) {
             alert({error: error.message});
