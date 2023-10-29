e6567a0ce7bb05b942348ed43479b4e298a6b79f
elvus <lvssgv@gmail.com>
2023-10-29 20:43:32 -0300

    ft: add register page

--- frontend/src/router/public_route.jsx
+++ frontend/src/router/public_route.jsx
@@ -1,11 +1,16 @@
 import { Navigate } from "react-router-dom";
 import Login from "../features/Login/Login";
+import Register from "../features/Register/Registro";
 
 export default function publicRoute() {
     return [
         { 
             path: "/login",
             element: <Login /> 
+        },
+        { 
+            path: "/register",
+            element: <Register /> 
         },
         {
             path: "*", 
