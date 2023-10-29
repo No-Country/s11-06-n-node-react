beed6c6d19c588e208aa63153a9365155b7dc42f
elvus <lvssgv@gmail.com>
2023-10-29 20:46:14 -0300

    ft: add register page

--- frontend/src/features/Login/Login.jsx
+++ frontend/src/features/Login/Login.jsx
@@ -49,7 +49,7 @@ const Login = () =>{
                     </div>
                     <div className='flex flex-col items-center mt-2.5'>
                         <div className='flex flex-wrap'>
-                            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
+                            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
                             <Link to="/register" className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Register</Link>
                         </div>
                     </div>
