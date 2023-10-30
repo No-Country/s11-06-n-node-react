ed3ad9bb244bd083ea403bacd0c3851d89cddce3
Guillermo Santolaya <guillermosantolaya.93@gmail.com>
2023-10-30 13:48:07 -0300

    modificando validaciones de eventos

--- back/routes/event-routes.js
+++ back/routes/event-routes.js
@@ -22,34 +22,13 @@ router.get("/:id", getEventById);
 router.get("/user/:userId", getUserEvents);
 
 // Ruta para crear un Evento
-router.post("/",
-    [
-        // Validaciones para la solicitud POST (createEvent)
-        body('name').notEmpty().isString(),
-        body('date').isISO8601().toDate(),
-        body('category').notEmpty().isString(),
-        body('locationLat').isNumeric(),
-        body('locationLong').isNumeric(),
-        body('image').isString(),
-    ],
-    validateErrors,
-    createEvent
-);
+router.post("/",createEvent);
 
 // Ruta para eliminar un Evento
 router.delete("/:id", deleteEvent);
 
 // Ruta para modificar todas las propiedades de un Evento
 router.put("/:id",
-    [
-        // Validaciones para la solicitud PUT (editEvent)
-        body('name').optional().isString(),
-        body('date').optional().isISO8601().toDate(),
-        body('category').optional().isString(),
-        body('locationLat').optional().isNumeric(),
-        body('locationLong').optional().isNumeric(),
-        body('image').optional().isString(),
-    ],
     validateErrors,
     editEvent
 );
