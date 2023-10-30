92a16fe7ec8e8de38c8559c9bc643359d756a1d0
elvus <lvssgv@gmail.com>
2023-10-30 09:32:17 -0300

    fix_conflicts

--- frontend/src/features/Login/Login.jsx
+++ frontend/src/features/Login/Login.jsx
@@ -3,6 +3,7 @@ import axios from "axios"
 import logo from '../../components/img/logo-1.png'
 import logoGoogle from '../../components/img/Google-Sign-In.png'
 import passwordIcon from '../../components/img/Vpn key.png'
+import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const Login = () =>{
@@ -99,9 +100,7 @@ const Login = () =>{
                             {errors.password && <span>{errors.password.message}</span>}
                         <button className='flex justify-start'>¿Olvidaste tu contraseña?</button>
                         <button type='submit' className='bg-[#098D82] border-2 border-[#098D82] rounded-md h-12 text-white'>Continuar</button>
-                        <button type='submit' className=''>Crear Cuenta</button>
-
-
+                        <Link to="/register" className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Crear Cuenta</Link>
                         </div>
                     </div>
                 </form>
