b423d658864a84a54576eda3dc1221df7a28cf11
JaviPeder~ <javierpedernera@gmail.com>
2023-11-01 15:48:34 -0300

    llevo info de grupos

--- frontend/src/features/Dashboard/components/Groups/Groups.jsx
+++
@@ -1,2 +0,0 @@
-//Aca mostraria todos los grupos
-
--- frontend/src/features/Grupos/pages/GruposPage.jsx
+++ frontend/src/features/Grupos/pages/GruposPage.jsx
@@ -2,83 +2,93 @@ import SearchBar from "../../../components/SearchBar/SearchBar";
 import Sidebar from "../../../components/Sidebar/Sidebar";
 import ListCardFirends from "../../../components/ListCardFriends";
 import CardGrupoPage from "../components/CardGrupoPage";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllGroups } from "../../../Redux/Actions/GroupGet";
+import { useEffect } from "react";
 
 export default function GruposPage() {
 
-    const gruposData = [
-        {
-          _id: 1,
-          description: "Argentinos en Punta Cana",
-          image:
-            "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-          users_common: [
-            {
-              _id: "653d0f88fa241077e51c4910",
-              name: "bot 1",
-              imageUser:
-                "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-            },
-            {
-              _id: "653d0f88fa241077e51c4910",
-              name: "bot 1",
-              imageUser:
-                "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-            },
-            {
-              _id: "653d0f88fa241077e51c4910",
-              name: "bot 1",
-              imageUser:
-                "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-            },
-          ],
-          users_admin: [
-            {
-              _id: "653d0f88fa241077e51c4910",
-              location: "Punta Cana",
-              name: "bot 1",
-              imageUser:
-                "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-            },
-          ],
-          date: "12/10/2023",
-        },
-        {
-            _id: 1,
-            description: "Argentinos en Italia",
-            image:
-              "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-            users_common: [
-              {
-                _id: "653d0f88fa241077e51c4910",
-                name: "bot 1",
-                imageUser:
-                  "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-              },
-              {
-                _id: "653d0f88fa241077e51c4910",
-                name: "bot 1",
-                imageUser:
-                  "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-              },
-              {
-                _id: "653d0f88fa241077e51c4910",
-                name: "bot 1",
-                imageUser:
-                  "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-              },
-            ],
-            users_admin: [
-              {
-                _id: "653d0f88fa241077e51c4910",
-                location: "Italia",
-                name: "bot 1",
-                imageUser:
-                  "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
-              },
-            ],
-            date: "12/10/2023",
-          },
-      ];
+  const AllGroups = useSelector((state) => state.group.groups);
+  const dispatch = useDispatch();
+  useEffect(() => {
+      dispatch(getAllGroups());
+      
+  }, []);
+console.log(AllGroups);
+    // const gruposData = [
+    //     {
+    //       _id: 1,
+    //       description: "Argentinos en Punta Cana",
+    //       image:
+    //         "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //       users_common: [
+    //         {
+    //           _id: "653d0f88fa241077e51c4910",
+    //           name: "bot 1",
+    //           imageUser:
+    //             "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //         },
+    //         {
+    //           _id: "653d0f88fa241077e51c4910",
+    //           name: "bot 1",
+    //           imageUser:
+    //             "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //         },
+    //         {
+    //           _id: "653d0f88fa241077e51c4910",
+    //           name: "bot 1",
+    //           imageUser:
+    //             "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //         },
+    //       ],
+    //       users_admin: [
+    //         {
+    //           _id: "653d0f88fa241077e51c4910",
+    //           location: "Punta Cana",
+    //           name: "bot 1",
+    //           imageUser:
+    //             "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //         },
+    //       ],
+    //       date: "12/10/2023",
+    //     },
+    //     {
+    //         _id: 1,
+    //         description: "Argentinos en Italia",
+    //         image:
+    //           "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //         users_common: [
+    //           {
+    //             _id: "653d0f88fa241077e51c4910",
+    //             name: "bot 1",
+    //             imageUser:
+    //               "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //           },
+    //           {
+    //             _id: "653d0f88fa241077e51c4910",
+    //             name: "bot 1",
+    //             imageUser:
+    //               "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //           },
+    //           {
+    //             _id: "653d0f88fa241077e51c4910",
+    //             name: "bot 1",
+    //             imageUser:
+    //               "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //           },
+    //         ],
+    //         users_admin: [
+    //           {
+    //             _id: "653d0f88fa241077e51c4910",
+    //             location: "Italia",
+    //             name: "bot 1",
+    //             imageUser:
+    //               "https://estaticos-cdn.sport.es/clip/96e3800b-93b4-4687-9dcb-470022f3f949_alta-libre-aspect-ratio_default_0.jpg",
+    //           },
+    //         ],
+    //         date: "12/10/2023",
+    //       },
+    //   ];
 
   return (
     <div>
@@ -86,9 +96,7 @@ export default function GruposPage() {
 
           <p>Grupo</p>
           <hr />
-    
-    
-          {gruposData.map((grupoData) => {
+          {AllGroups.map((grupoData) => {
              return (
               <CardGrupoPage
                 key={grupoData._id}
