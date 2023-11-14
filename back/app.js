42e638eae835154c419ac77686dedf557902a68a
Gustavo Torres <95319442+betogus@users.noreply.github.com>
2023-11-13 21:48:19 -0300

    Update app.js

--- back/app.js
+++ back/app.js
@@ -62,21 +62,8 @@ const sess = {
 };
 
 initializePassport();
-const allowedOrigins = ['http://localhost:5173', 'https://s11-06-n-node-react.vercel.app', 'http://localhost:8080'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors({ origin: '*' }));
 app.use(session(sess));
 app.use(passport.initialize())
 app.use(passport.session())
